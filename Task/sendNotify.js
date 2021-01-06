f1f2dd1712a27cd4289d0b1ea65228a11e0e687a
forpw2009 <731588359@qq.com>
2021-01-06 19:53:15 +0800

    Update sendNotify.js

--- Task/sendNotify.js
+++ Task/sendNotify.js
@@ -2,7 +2,7 @@ const $ = new Env();
 // =======================================微信server酱通知设置区域===========================================
 //此处填你申请的SCKEY.
 //注：此处设置github action用户填写到Settings-Secrets里面(Name输入PUSH_KEY)
-let SCKEY = '';
+let SCKEY = 'SCU8385T78718e8fa89985ed009076e57bda5cc15919c4393117c';
 
 // =======================================Bark App通知设置区域===========================================
 //此处填你BarkAPP的信息(IP/设备码，例如：https://api.day.app/XXXXXXXX)
